1ae125eb30b7790504820fd688fe7e9d7da160a9
Bryce Boe <bbzbryce@gmail.com>
2019-03-08 09:31:57 -0800

    Run 'black .' to deterministically format the code

--- setup.py
+++ setup.py
@@ -5,30 +5,33 @@ from os import path
 from setuptools import setup
 
 
-PACKAGE_NAME = 'deterministic_zip'
+PACKAGE_NAME = "deterministic_zip"
 HERE = path.abspath(path.dirname(__file__))
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as fp:
+with open(path.join(HERE, "README.md"), encoding="utf-8") as fp:
     README = fp.read()
-with open(path.join(HERE, PACKAGE_NAME, '__init__.py'),
-          encoding='utf-8') as fp:
+with open(
+    path.join(HERE, PACKAGE_NAME, "__init__.py"), encoding="utf-8"
+) as fp:
     VERSION = re.search("__version__ = '([^']+)'", fp.read()).group(1)
 
 
-setup(name=PACKAGE_NAME,
-      author='Bryce Boe',
-      author_email='bbzbryce@gmail.com',
-      classifiers=[
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: BSD License',
-          'Programming Language :: Python :: 3.7'],
-      description='A program to create deterministic zip files.',
-      entry_points={'console_scripts': ['{0} = {0}:main'
-                                        .format(PACKAGE_NAME)]},
-      extras_require={'dev': ['black', 'flake8', 'pre-commit']},
-      keywords='aws lambda zip',
-      license='Simplified BSD License',
-      long_description=README,
-      long_description_content_type='text/markdown',
-      packages=[PACKAGE_NAME],
-      url='https://github.com/bboe/deterministic_zip',
-      version=VERSION)
+setup(
+    name=PACKAGE_NAME,
+    author="Bryce Boe",
+    author_email="bbzbryce@gmail.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="A program to create deterministic zip files.",
+    entry_points={"console_scripts": ["{0} = {0}:main".format(PACKAGE_NAME)]},
+    extras_require={"dev": ["black", "flake8", "pre-commit"]},
+    keywords="aws lambda zip",
+    license="Simplified BSD License",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=[PACKAGE_NAME],
+    url="https://github.com/bboe/deterministic_zip",
+    version=VERSION,
+)
